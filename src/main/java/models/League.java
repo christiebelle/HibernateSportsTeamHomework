0fc6c229afc6d549037c2eb1864a0e823576e14b
Christiebelle <cslester@outlook.com>
2018-03-16 12:01:17 +0000

    League class constructor, getters and setters created

--- src/main/java/models/League.java
+++ src/main/java/models/League.java
@@ -1,4 +1,61 @@
 package models;
 
+import java.util.GregorianCalendar;
+import java.util.HashSet;
+import java.util.Set;
+
 public class League {
+
+    private int id;
+    private String name;
+    private GregorianCalendar seasonstart;
+    private GregorianCalendar seasonend;
+    private Set<Team> teams;
+
+    public League(String name, GregorianCalendar seasonstart, GregorianCalendar seasonend) {
+        this.name = name;
+        this.seasonstart = seasonstart;
+        this.seasonend = seasonend;
+        this.teams = new HashSet<Team>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public GregorianCalendar getSeasonstart() {
+        return seasonstart;
+    }
+
+    public void setSeasonstart(GregorianCalendar seasonstart) {
+        this.seasonstart = seasonstart;
+    }
+
+    public GregorianCalendar getSeasonend() {
+        return seasonend;
+    }
+
+    public void setSeasonend(GregorianCalendar seasonend) {
+        this.seasonend = seasonend;
+    }
+
+    public Set<Team> getTeams() {
+        return teams;
+    }
+
+    public void setTeams(Set<Team> teams) {
+        this.teams = teams;
+    }
 }
